bbb84fbdfde1f3c0a4e9630c732120baf581dc7a
Rohan <rshirsalkar5@gmail.com>
2022-04-09 14:08:46 +0530

    Removed Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,17 +13,17 @@ export default function Navbar() {
                             TextFun
                         </Link>
 
-                        <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                        {/* <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                             <li><Link to="/" className="nav-link px-2 text-white">Home</Link></li>
                             <li><Link to="/features" className="nav-link px-2 text-white">Features</Link></li>
                             <li><Link to="/pricing" className="nav-link px-2 text-white">Pricing</Link></li>
-                            {/* <li><Link to="#" className="nav-link px-2 text-white">About</Link></li> */}
-                        </ul>
+                            <li><Link to="#" className="nav-link px-2 text-white">About</Link></li> 
+                        </ul> */}
 
-                        <div className="form-check form-switch">
+                        {/* <div className="form-check form-switch">
                             <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
                             <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </header>
